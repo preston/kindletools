2af9baa78795ce9861f5f2d66767b7315a917a7c
Preston Lee <conmotto@gmail.com>
2010-02-02 04:23:17 -0700

    updating cap scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,3 +27,27 @@ namespace :deploy do
     run "touch #{current_release}/tmp/restart.txt"
   end
 end
+
+
+namespace :config do
+ 
+  desc "Add server-only shared directories."
+  task :setup, :roles => [:app] do
+  	run "mkdir -p #{shared_path}/config"
+    run "mkdir -p #{shared_path}/db"
+  end
+  after "deploy:setup", "config:setup"
+  
+  desc "Update server-only config files to new deployment directory."
+  task :update, :roles => [:app] do
+    run "cp -Rf #{shared_path}/config #{release_path}"
+  end
+  after "deploy:update_code", "config:update"
+ 
+  desc "Update shared server database symlink for new deployment."
+  task :database, :roles => [:app] do
+    run "ln -s #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+  end
+  after "deploy:update_code", "config:update"
+
+end
