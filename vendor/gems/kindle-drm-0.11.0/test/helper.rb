1eaada94fb565b023f641ca9373520e7732340b4
Preston Lee <conmotto@gmail.com>
2010-04-27 13:28:01 -0700

    Updating for Kindle DX 2.

---
+++ vendor/gems/kindle-drm-0.11.0/test/helper.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'kindle-drm'
+
+include Kindle::DRM
+
+class Test::Unit::TestCase
+end
