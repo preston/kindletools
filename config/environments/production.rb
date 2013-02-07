1865d217b5d82943e21e49ad66db6afe3e1669a2
Preston Lee <conmotto@gmail.com>
2013-02-07 14:33:38 -0700

    Updating to latest stable rails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ KindleTools::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Defaults to Rails.root.join("public/assets")
+  # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
