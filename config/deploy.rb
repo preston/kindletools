28626cd8cd452d4862f37b8f89378babf40b90d0
Preston Lee <conmotto@gmail.com>
2013-11-08 20:35:46 -0700

    Upgrading to Capistrano v3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,20 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'kindletools'
+set :repo_url, 'git@github.com:preston/kindletools.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :deploy_to, "/var/www/kindletools"
+set :scm, :git
 
 # set :format, :pretty
-# set :log_level, :debug
+set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 3
 
 namespace :deploy do
 
@@ -22,7 +22,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
