62c3493db7626cde2f35c0dac6de96d908bd9f7b
Preston Lee <conmotto@gmail.com>
2015-04-13 16:37:15 -0700

    Dependency updates.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,20 +13,11 @@ set :log_level, :debug
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-after 'deploy:publishing', 'deploy:restart'
-
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 3
 
-namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -36,6 +27,7 @@ namespace :deploy do
       # end
     end
   end
+
 
   after :finishing, 'deploy:cleanup'
 
